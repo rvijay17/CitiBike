93b903ce17e1bfd5051accf65c5ed8d8c2a9b0ba
Vijay Rajagopalan <rvijaysubs@gmail.com>
2016-05-29 02:44:55 +1000

    Added 2014 temperature data, updated initial analysis notebook, updated model evaluation notebook

--- src/CitiBike_ETL.py
+++ src/CitiBike_ETL.py
@@ -8,18 +8,14 @@ import feather
 import glob
 
 dirname = '../../../Data/CitiBike_Data/'
-# filename = '201506-citibike-tripdata.csv'
-filename = '20*-citibike-tripdata.csv'
-weather_file = '../data/temperature/weather.csv'
+filename = '201*.csv'
 feather_output_filename = 'bikedata.feather'
-public_holiday_file = 'nyc_public_holiday.csv'
 listFiles = []
 
 # Read the data files
 for fname in glob.glob(dirname + filename):
     bikedata = pd.read_csv(fname)
 
-    # Data manipulation
     # Only have data where gender is either 1 or 2
     bikedata = bikedata[bikedata['gender'].isin([1,2])]
 
@@ -33,25 +29,36 @@ for fname in glob.glob(dirname + filename):
     try:
         bikedata['dtstarttime'] = pd.to_datetime(bikedata.starttime, format="%m/%d/%Y %H:%M:%S")
     except ValueError:
-        bikedata['dtstarttime'] = pd.to_datetime(bikedata.starttime, format="%m/%d/%Y %H:%M")
+        try:
+            bikedata['dtstarttime'] = pd.to_datetime(bikedata.starttime, format="%m/%d/%Y %H:%M")
+        except ValueError:
+            bikedata['dtstarttime'] = pd.to_datetime(bikedata.starttime, format="%Y-%m-%d %H:%M:%S")
 
-    try:
-        bikedata['dtstoptime'] = pd.to_datetime(bikedata.stoptime, format="%m/%d/%Y %H:%M:%S")
-    except ValueError:
-        bikedata['dtstoptime'] = pd.to_datetime(bikedata.stoptime, format="%m/%d/%Y %H:%M")
+    # Note required as stop date is not used in the model
+    # try:
+    #     bikedata['dtstoptime'] = pd.to_datetime(bikedata.stoptime, format="%m/%d/%Y %H:%M:%S")
+    # except ValueError:
+    #     try:
+    #         bikedata['dtstoptime'] = pd.to_datetime(bikedata.stoptime, format="%m/%d/%Y %H:%M")
+    #     except ValueError:
+    #         bikedata['dtstoptime'] = pd.to_datetime(bikedata.stoptime, format="%Y-%m-%d %H:%M:%S")
 
     # Set the startdate and stopdate - minutes and seconds reset to 0 (in the following format - 2016-03-01 06:00:00)
     # This has been done so that we could aggregate departures and arrivals per hour to identify bike usage
     bikedata['dtstartdatehour'] = bikedata.dtstarttime.apply(lambda x:x.replace(minute=0,second=0))
-    bikedata['dtstopdatehour'] = bikedata.dtstoptime.apply(lambda x:x.replace(minute=0,second=0))
-    bikedata['startdatehour'] = bikedata.dtstartdatehour.apply(lambda x:x.strftime('%Y-%m-%d %H:%M:%S'))
+
+    ## dtstopdatehour is not required as stopdate is not used in the model
+    # bikedata['dtstopdatehour'] = bikedata.dtstoptime.apply(lambda x:x.replace(minute=0,second=0))
+
+    ## The following two columns are handled in the notebook itself. The new column is created after the aggregation
+    # bikedata['startdatehour'] = bikedata.dtstartdatehour.apply(lambda x:x.strftime('%Y-%m-%d %H:%M:%S'))
     # bikedata['startdate'] = bikedata.dtstarttime.dt.date.apply(lambda x:x.strftime('%Y-%m-%d'))
 
     # datetime.datetime columns are not supported by Feather
     # So, deleting the column after pre-processing.
     # TODO: Can we do without converting the dates in the first place?
     bikedata.drop('dtstarttime', axis=1, inplace=True)
-    bikedata.drop('dtstoptime', axis=1, inplace=True)
+    # bikedata.drop('dtstoptime', axis=1, inplace=True)
 
     print fname, ' - ', len(bikedata)
 
--- src/Wunderground_History_Temp.py
+++ src/Wunderground_History_Temp.py
@@ -1,5 +1,6 @@
 # coding: utf-8
-# Wunderground has a limit of 10 API calls per minute. To be on the safer side, I'll make 10 calls every 150 seconds
+# Wunderground has a limit of 10 API calls per minute.
+# To be on the safer side, I'll make 10 calls every 'sleep_for' seconds
 # So, there is a wait after every 5 calls
 
 import urllib2
@@ -9,23 +10,24 @@ import string
 import time
 from datetime import datetime
 
-year = [2015]
-month = [1,2,3]
+year = [2014]
+month = [3,4,5,6,7,8,9,10,11,12]
 
 target_dir = '../data/temperature/raw_data/'
+sleep_for = 75
 
 for y in year:
     for m in month:
-        # Let's sleep for 90 seconds before we start with the next month
-        print 'Sleeping for 90 seconds ...'
-        time.sleep(90)
+        # Let's sleep before we start with the next month
+        print 'Sleeping for', sleep_for, 'seconds ...'
+        time.sleep(sleep_for)
 
         max_date = cal.monthrange(y, m)[1]
         for i in xrange(max_date):
 
             if (i+1) % 10 == 0:
-                print 'Sleeping for 90 seconds ...'
-                time.sleep(90)
+                print 'Sleeping for', sleep_for, 'seconds ...'
+                time.sleep(sleep_for)
 
             s = str(y) + string.zfill(str(m), 2) + string.zfill(str(i+1), 2)
             url = 'http://api.wunderground.com/api/0020b1868a2c32c4/history_' + s + '/q/NY/New_York.json'
